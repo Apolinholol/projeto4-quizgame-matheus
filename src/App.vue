1ae9c95a934a0f191948c19843e3f4c358a86a61
Apolinholol <matheuslcneves@gmail.com>
2024-10-16 16:57:55 -0300

    é isso

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    
+  </div>
 </template>
 
 <script lang="ts">
+
+import axios from 'axios';
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  created() {
+    axios
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(response => console.log(response.data))
+  },
+
+  });
 </script>
 
 <style lang="scss">
